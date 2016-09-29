0c8a4e7cf02ef639950fe3477b60d4f4b4e09838
eiraxue <eirabenxue@gmail.com>
2016-09-29 15:25:09 -0400

    added plot for return and volatility

--- hmm.py
+++ hmm.py
@@ -21,3 +21,17 @@ end = datetime.datetime(2016, 1, 27)
 data = data.DataReader("GOOGL", 'yahoo', start, end)
 #('CSI300.INDX', start_date = beginDate, end_date=endDate, frequency='1d')
 print data.head()
+
+
+import numpy as np
+import pandas as pd
+from pandas_datareader import data, wb
+import matplotlib
+
+goog = data.DataReader('goog', data_source='google', start='3/14/2009', end='4/14/2014')
+goog.tail()
+
+goog['Log_Ret'] = np.log(goog['Close']/goog['Close'].shift(1))
+goog['Volatility'] = pd.rolling_std(goog['Log_Ret'], window=252)*np.sqrt(252)
+%matplotlib inline
+goog[['Close', 'Volatility']].plot(subplots=True, color='blue', figsize=(8,6))
